037667c1e817038ec625cf98729eb87e7914137e
mudigone <76899523+mudigone@users.noreply.github.com>
2022-06-02 15:09:45 +0500

    Revert "NFT-Collection DApp"
    This reverts commit 8bab689bc3a671b48cfba996c8a0fb43dd4be0c2.

--- hardhat-tutorial/constants/index.js
+++
@@ -1,7 +0,0 @@
-// Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0x81a259A77056C63b92433a64dA7f96E224E165E9";
-// URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
-
-module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
-        
